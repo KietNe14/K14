5a5119fafe2dfa7f6027eeb7cd4f0fb55b5c5651
KietNe14 <93403489+KietNe14@users.noreply.github.com>
2025-04-02 14:22:45 +0700

    Update __manifest__.py

--- custom_ms_office_preview/__manifest__.py
+++ custom_ms_office_preview/__manifest__.py
@@ -8,7 +8,7 @@
     It extends the attachment model to recognize Office file types and provides a seamless viewing experience 
     by embedding the Office Viewer in the file preview interface.
     """,
-    "depends": ["website"],
+    "depends": ["web"],
     "data": [
     ],
     "assets": {
